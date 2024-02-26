4c78ebd7dde55b2400d97bdd10e4d63a6c74d9e1
Alter <xm5960698@gmail.com>
2024-02-26 17:52:24 +0800

    成员属性

--- 成员属性/Program.cs
+++ 成员属性/Program.cs
@@ -1,10 +1,54 @@
 ﻿namespace 成员属性
 {
+    class Hero
+    {
+        int hp;
+        public int Id { get; set; }
+        public int Hp
+        {
+            get
+            {
+                return hp; // 这里不能返回Hp 那样就变成递归调用了 
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    hp = 0;
+                }
+                else
+                {
+                    hp = value;
+                }
+            }
+        }
+        public string State { get; set; }
+        public string Description { get; set; }
+        public int Amount { get; set; }
+        public int Balance { get; set; }
+
+        public Hero() : this(10)
+        { }
+        public Hero(int hp)
+        {
+            Hp = hp;
+        }
+        public void GetHurt(int dmg)
+        {
+            Hp -= dmg;
+            if (Hp == 0) this.State = "Dead";
+        }
+
+
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Hero hero1 = new Hero();
+            hero1.GetHurt(20);
+            Console.WriteLine(hero1.State);
         }
     }
 }
