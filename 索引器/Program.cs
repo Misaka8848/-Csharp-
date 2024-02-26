01c8e9af8465a574b65063bf721643b6520cf775
Alter <xm5960698@gmail.com>
2024-02-26 19:41:15 +0800

    索引器

---
+++ 索引器/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace 索引器
+{
+    class Team
+    {
+        string[] arr = new string[10];
+        public string this[int index]
+        {
+            get
+            {
+                return arr[index];
+            }
+            set
+            {
+                arr[index] = value;
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
