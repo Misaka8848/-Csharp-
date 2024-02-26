0eda5514bcdb70bcdebffaadaefd5c3a0351c13e
Alter <xm5960698@gmail.com>
2024-02-26 16:27:18 +0800

    跳过直到11节成员属性

---
+++ 成员属性/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace 成员属性
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ 类和对象/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace 类和对象
+{
+    internal class Program
+    {
+        class People
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            People people; //s声明
+            people = new People();//构造函数初始化
+
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
