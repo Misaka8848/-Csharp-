3ec43217e68c4126b9765d886faf329af7f2c11f
Alter <xm5960698@gmail.com>
2024-02-26 20:44:31 +0800

    静态成员

---
+++ 静态成员/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace 静态成员
+{
+    //静态类用于工具集
+    static class MyUtilities
+    {
+        public static void ClearMeg()
+        {
+            // 清理工作台
+        }
+    }
+    class Config
+    {
+        // 静态字段 用于在同一个类的所有实例之间共享值
+        static string dataBaseAdress = "123123";
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
